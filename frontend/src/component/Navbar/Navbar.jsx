3fe1fe98d7308f3bb26eda904c104f3854b0e63f
Yati Tiwari <tiwariyati221@gmail.com>
2026-03-02 19:34:53 +0530

    Initial commit

---
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { IconButton } from '@mui/material'
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+
+export const Navbar=()=>{
+    return(
+        <div className='w-screen left-0 fixed top-0 px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
+
+                <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
+                    <div className='logo font-semibold text-gray-300 text-2xl'>
+                        Sphere food
+                    </div>
+                     
+                </div>
+
+            <div className='flex items-center space-x-2 lg:space-x-10'>
+                <div className=''>
+                    <IconButton>
+                        <SearchIcon sx={{fontSize:"1.5rem"}}/>
+                    </IconButton>
+
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
