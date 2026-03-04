bbcc49a04e1ee947f89497aed87f0fe17af54374
vanisha-mittal <vanishamittal1409@gmail.com>
2026-03-04 15:14:14 +0530

    cors

---
+++ backend/OrderSphere/OrderSphere/src/main/java/com/orderSphere/OrderSphere/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.orderSphere.OrderSphere.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="ndjfeliuferhjfkernfgilvefjiergfej;lesdfkerofjiek;lvsd;";
+    public static final String JWT_HEADER="Authorization";
+}
