d321cf58dfa6bfdaab7038ab6587d54a7a89b16d
vanisha-mittal <vanishamittal1409@gmail.com>
2026-03-09 11:38:32 +0530

    card

--- frontend/src/component/Profile/EventCard.jsx
+++ frontend/src/component/Profile/EventCard.jsx
@@ -22,12 +22,12 @@ export const EventCard = () => {
             </div>
         </CardContent>
 
-        { false && (<CardActions>
+        { <CardActions>
             <IconButton>
                 <DeleteIcon/>
             </IconButton>
 
-        </CardActions>)}
+        </CardActions>}
       </Card>
     </div>
   )
